988f34eaeec79cad84d585c62402855f15d8e5ab
Danny Rusén <danny.rusen@arkido.se>
2025-12-03 15:09:54 +1300

    Performance improvements

--- 2025/Day2/part2.py
+++ 2025/Day2/part2.py
@@ -2,52 +2,48 @@ import time
 
 from input import ids
 
-def get_id_range(id_range: str) -> list[int]:
+def get_id_range(id_range: str) -> range:
     start, end = map(int, id_range.split("-"))
-    return list(range(start, end + 1))
+    return range(start, end + 1)
 
 def check_valid_id(product_id: int) -> bool:
     s = str(product_id)
     n = len(s)
 
-    is_false = False
-    for i in range(1, n + 1):
-        if not check_sequence(s, i):
-            is_false = True
-            break
-
-    if is_false:
-        return False
+    for i in range(1, n // 2 + 1):
+        if check_sequence(s, i):
+            return False
 
     return True
 
 def check_sequence(s: str, length: int) -> bool:
-    str_part = s[0:length]
-
-    if str_part == s:
+    if length == 0:
         return True
 
-    for i in range(length, len(s), length ):
-        if str_part != s[i:i+length]:
-            return True
+    if len(s) % length != 0:
+        return False
 
-    return False
+    num_repeats = len(s) // length
+    return s == s[:length] * num_repeats
 
 def main():
     input_ids = ids.split(",")
 
     start_time = time.perf_counter()
 
-    all_ids = [id_to_check
-               for input_id in input_ids
-               for id_to_check in get_id_range(input_id)]
+    invalid_ids_sum = 0
+    invalid_ids_count = 0
 
-    invalid_ids = [id for id in all_ids if not check_valid_id(id)]
+    for id_range_str in input_ids:
+        for id_to_check in get_id_range(id_range_str):
+            if not check_valid_id(id_to_check):
+                invalid_ids_sum += id_to_check
+                invalid_ids_count += 1
 
     end_time = time.perf_counter()
 
-    print(f"Failed IDs: {invalid_ids}")
-    print(f"Sum Failed IDs: {sum(invalid_ids)}")
+    print(f"Found {invalid_ids_count} invalid IDs.")
+    print(f"Sum Failed IDs: {invalid_ids_sum}")
 
     print(f"Total execution time: {end_time - start_time:.6f}s")
 
